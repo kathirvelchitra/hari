50271eacff94b0c293ac35690dba546cfb9b63eb
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-01-19 08:36:34 +0000

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+# hari#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the value");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("odd",a);
+    }
+    else
+    {
+        printf("even",a);
+    }
+}
